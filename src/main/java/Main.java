ddd51dae509641572243ab891c9961fca241136c
Meredith <meredith@Zipcoders-MacBook-Pro.local>
2022-03-01 20:44:21 -0500

    lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.println;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
+    public static String getIntInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        String inputString = scanner.nextLine();
+        return inputString;
+    }
+
     public static void main(String[] args){
-        
+        int gameStatus = 0;
+        int number = (int) ((Math.random() * (100 - 1)) + 1); // https://www.baeldung.com/java-generating-random-numbers-in-range
+        int guesses = 0;
+        while (gameStatus == 0) {
+        String s = Main.getIntInput("Guess the number between 1 and 100!");
+        int inputNum = Integer.parseInt(s); // https://www.javatpoint.com/java-string-to-int
+            if (inputNum == number) {
+                guesses++;
+                System.out.println("Correct!");
+                System.out.println("Total guesses: " + guesses);
+                gameStatus = 1;
+            } else if (inputNum > number) {
+                guesses++;
+                System.out.println("too large");
+            } else if (inputNum < number) {
+                guesses++;
+                System.out.println("too small");
+            }
+        }
     }
 }
